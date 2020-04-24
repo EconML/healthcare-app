0685aae5662631fe3fa3954d8e573c8709598808
econml <staplema@plu.edu>
2020-04-23 18:21:14 -0700

    added js to populate sidebar

--- frontend/static/frontend/js/app.js
+++ frontend/static/frontend/js/app.js
@@ -1,25 +1,50 @@
-
+// document ready function requires the page to be loaded before jquery
 $(document).ready(function () {
 	let columns = {};
+	//the below toggles the sidebar on click
 	$('.left-collapse').on('click', function () {
 		$('.sidebar').toggleClass('active');
 		$(this).toggleClass('active');
 	});
-	$('.dropdown-toggle').click( function() {
-		if( $(this).attr( 'aria-expanded' ) == "false" ){
-			let id = $(this).attr( 'href' );
-			if( !columns[id] ){
-				$(id + " li a").each(function(item){
-					$("#columns").append( "<th class="+id.substring(1)+">"+$(this).html()+"</th>" );
-				} );
-				columns[id] = true;
-			}
-		} else {
-			let id = $(this).attr( 'href' );
-			if( columns[id] ){
-				$("."+id.substring(1)).remove();
-				columns[id] = false;
-			}
-		}
-	} );
 });
+
+buildList()
+
+function buildList(){
+	var conditionList = document.getElementById('conditionList')
+	conditionList.innerHTML = ''
+	var url = 'http://localhost:8000/api/v1/clinicalmeasures/'
+
+	fetch(url)
+	.then((resp) => resp.json())
+	.then(function(data){
+		console.log('Data:', data)
+
+		var clinicalmeasureconditions = data
+		for(var i in clinicalmeasureconditions){
+			var condition = `
+					<li>
+						<a href="#diabeticSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">${clinicalmeasureconditions[i].id}</a>
+						<ul class="collapse list-unstyled" id="diabeticSubmenu">
+							<li>
+								<a href="#">Diabetes HgbA1c testing</a>
+							</li>
+							<li>
+								<a href="#">Diabetes Retinal Eye Exam</a>
+							</li>
+							<li>
+								<a href="#">Diabetes Foot Exam</a>
+							</li>
+						</ul>
+					</li>
+			`
+
+			conditionList.innerHTML += condition
+
+
+			console.log(clinicalmeasureconditions[i].id)
+			console.log(clinicalmeasureconditions[i].resource)
+
+		}
+	})
+}
